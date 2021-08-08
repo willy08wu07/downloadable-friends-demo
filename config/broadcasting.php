89f17f642cda60d36df9455985be821f4e6b7d72
willy08wu07 <willy08wu07@users.noreply.github.com>
2021-08-08 16:59:22 +0800

    Config events broadcasting connection
    Reference: <https://beyondco.de/docs/laravel-websockets/basic-usage/pusher#pusher-configuration>

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,6 +37,8 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => 'localhost',
+                'port' => env('LARAVEL_WEBSOCKETS_PORT'),
                 'useTLS' => true,
             ],
         ],

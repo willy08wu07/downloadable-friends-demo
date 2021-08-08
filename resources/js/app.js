a14efbbdb8994d1e452ef472226cd372633d856c
willy08wu07 <willy08wu07@users.noreply.github.com>
2021-08-09 00:32:29 +0800

    Implement basic front-end appearance & behavior

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,81 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import {nextTick} from "vue";
+
+const App = {
+    data() {
+        return {
+            messages: [],
+            author_name: '',
+            input_message: '',
+        }
+    },
+    created() {
+        this.fetchMessages()
+    },
+    computed: {
+        validation_error() {
+            if (this.author_name.trim().length === 0) {
+                return '請輸入暱稱'
+            } else if (this.author_name.length > 20) {
+                return '暱稱太長'
+            } else if (this.input_message.trim().length === 0) {
+                return '請輸入訊息'
+            } else if (this.input_message.length > 100) {
+                return '訊息太長'
+            }
+            return ''
+        }
+    },
+    methods: {
+        fetchMessages() {
+            axios.get('/messages').then(response => {
+                this.messages = response.data.data.reverse().map(e => this.convertToPresenter(e))
+                this.forceScrollDown()
+            })
+            Echo.channel('public')
+                .listen('ChatMessagePosted', (e) => {
+                    this.scrollDownWhenNeededAfter(() => {
+                        this.messages.push(this.convertToPresenter(e.chatMessage))
+                    })
+                })
+        },
+        postNewMessage() {
+            let newMessage = {
+                author_name: this.author_name.trim(),
+                message: this.input_message.trim(),
+            }
+            this.input_message = ''
+            axios.post('/messages', newMessage).then(response => {
+                let data = this.convertToPresenter(response.data.data)
+                data.is_my_message = true
+                this.messages.push(data)
+                // 既然是自己貼的訊息，強制捲動到最下面。
+                this.forceScrollDown()
+            })
+        },
+        convertToPresenter(chatMessage) {
+            chatMessage.is_my_message = false
+            chatMessage.created_at = new Date(chatMessage.created_at).toLocaleString()
+            return chatMessage
+        },
+        scrollDownWhenNeededAfter(addMessagesAction) {
+            let div = this.$refs.chatMessages
+            // 若使用者正在瀏覽前面的訊息，不要隨意幫他捲動到最下面。門檻值 100 像素。
+            let isScrollDownNeeded = div.scrollTop > div.scrollHeight - div.clientHeight - 100
+            addMessagesAction()
+            if (isScrollDownNeeded) {
+                this.forceScrollDown()
+            }
+        },
+        forceScrollDown() {
+            nextTick(() => {
+                let div = this.$refs.chatMessages
+                div.scrollTop = div.scrollHeight - div.clientHeight
+            })
+        }
+    }
+}
+document.addEventListener("DOMContentLoaded", () => {
+    Vue.createApp(App).mount('#app')
+})
--- resources/views/chat-room.blade.php
+++ resources/views/chat-room.blade.php
@@ -8,5 +8,43 @@
     <link href="/css/app.css" rel="stylesheet">
 </head>
 <body class="font-sans">
+<div id="app" class="w-full h-screen flex-col grid grid-cols-1 lg:grid-cols-4 bg-gray-200 dark:bg-gray-800">
+    <div ref="chatMessages" class="row-start-1 lg:col-start-2 lg:col-span-2 overflow-y-auto">
+        <div v-for="message in messages">
+            <div v-if="!message.is_my_message" v-bind:lang="message.lang" class="grid grid-cols-3 p-2">
+                <div class="col-start-1 col-span-2 text-gray-700 dark:text-gray-300 ml-2">
+                    @{{ message.author_name }}
+                </div>
+                <div dir="auto" class="col-start-1 col-span-2">
+                    <div class="float-left inline-block bg-purple-400 dark:bg-purple-800 text-gray-900 dark:text-gray-100 shadow-md rounded-lg text-lg pt-1 pb-1 pl-2 pr-2">
+                        @{{ message.message }}
+                    </div>
+                </div>
+                <div class="col-start-1 col-span-2 text-gray-400 dark:text-gray-600 ml-2">
+                    @{{ message.created_at }}
+                </div>
+            </div>
+            <div v-if="message.is_my_message" v-bind:lang="message.lang" class="grid grid-cols-3 p-2">
+                <div class="col-end-4 col-span-2 text-gray-700 dark:text-gray-300 ml-2 text-right">
+                    @{{ message.author_name }}
+                </div>
+                <div dir="auto" class="col-end-4 col-span-2">
+                    <div class="float-right inline-block bg-yellow-300 dark:bg-yellow-700 text-gray-900 dark:text-gray-100 shadow-md rounded-lg text-lg pt-1 pb-1 pl-2 pr-2">
+                        @{{ message.message }}
+                    </div>
+                </div>
+                <div class="col-end-4 col-span-2 text-gray-400 dark:text-gray-600 ml-2 text-right">
+                    @{{ message.created_at }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="input" class="flex row-start-2 lg:col-start-2 lg:col-span-2">
+        <input type="text" placeholder="您的暱稱" v-model="author_name" class="w-20 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex-none p-2">
+        <input type="text" dir="auto" placeholder="想分享什麼心情或想法？" v-model="input_message" class="w-40 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex-auto p-2">
+        <button v-if="validation_error" class="bg-purple-400 dark:bg-purple-800 text-gray-900 dark:text-gray-100 flex-none p-2">@{{ validation_error }}</button>
+        <button v-if="!validation_error" v-on:click="postNewMessage" class="bg-yellow-300 dark:bg-yellow-700 text-gray-900 dark:text-gray-100 flex-none p-2">送出訊息</button>
+    </div>
+</div>
 </body>
 </html>
